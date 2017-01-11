5f79a46b88eda1ada7877366b7c8d4baa28e7885
mgieysztor <michal.gieysztor@gmail.com>
2017-01-11 21:00:00 +0100

    ciąg dalszy

--- src/main/java/pl/sda/Test.java
+++ src/main/java/pl/sda/Test.java
@@ -1,10 +1,19 @@
 package pl.sda;
 
-/**
- * Created by RENT on 2017-01-11.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) throws InterruptedException {
-        Thread.sleep(120000);
+        List<Integer> integers = new ArrayList<>();
+
+
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            Thread.sleep(1);
+            for (int j = 0; j <100000 ; j++) {
+                Integer integer = new Integer(10);
+            }
+        }
+        Thread.sleep(3000000);
     }
 }
