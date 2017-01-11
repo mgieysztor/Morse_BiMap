7aa10501d79c7d750b2ef265fe4399737612ae78
mgieysztor <michal.gieysztor@gmail.com>
2017-01-11 19:50:17 +0100

    Właściwie działa, na pewno z Alpha do Morse.

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        String converted = TextToMorseConverter.convert("Jakis napis");
+        String converted = TextToMorseConverter.convert("SOS");
         System.out.println("Morse code");
         System.out.println(converted);
     }
--- src/main/java/pl/sda/TextToMorseConverter.java
+++ src/main/java/pl/sda/TextToMorseConverter.java
@@ -1,20 +1,36 @@
 package pl.sda;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
 /**
  * Created by RENT on 2017-01-11.
  */
 
 public class TextToMorseConverter {
-
+    public static String[] alpha = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+            "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8",
+            "9", "0", " "};
+    public static String[] dottie = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+            "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+            "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+            "-.--", "--..", ".----", "..---", "...--", "....-", ".....",
+            "-....", "--...", "---..", "----.", "-----", "|"};
+    public static BiMap<String, String> alphaMorse = HashBiMap.create();
+    static {
+        for (int i = 0; i < alpha.length; i++) {
+            alphaMorse.put(alpha[i], dottie[i]);
+        }
+    }
 
     public static String convert(String text) {
         String[] split = text.split("");
 
-
         StringBuilder morseString = new StringBuilder();
 
         for (String element : split) {
-            String convertedChar = convertChar(element);
+            String convertedChar = convertChar(element,true);
             morseString.append(convertedChar);
             morseString.append(" ");
         }
@@ -22,45 +38,13 @@ public class TextToMorseConverter {
         return morseString.toString();
     }
 
-    public static String convertChar(String charToConvert) {
-//        switch (charToConvert){
-//            case "a":
-//                return ".-";
-//            case "b":
-//                return "-...";
-//        }
-//
-//        List<TextMorsePair> textMorsePairs = new ArrayList<TextMorsePair>();
-//        textMorsePairs.add(new TextMorsePair("a", ".-"));
-//        textMorsePairs.add(new TextMorsePair("a", ".-"));
+    public static String convertChar(String charToConvert, boolean toMorse) {
 
-        String[] alpha = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-                "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-                "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8",
-                "9", "0", " "};
-        String[] dottie = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
-                "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
-                "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
-                "-.--", "--..", ".----", "..---", "...--", "....-", ".....",
-                "-....", "--...", "---..", "----.", "-----", "|"};
-
-
-//        charToConvert.toLowerCase();
-        charToConvert = charToConvert.toLowerCase();
-
-        int index = -1;
-        for (int i = 0; i < alpha.length; i++) {
-            String current = alpha[i];
-            if (current.equals(charToConvert)) {
-                index = i;
-                break;
-            }
-        }
-
-        if (index != -1) {
-            return dottie[index];
+        if (toMorse) {
+            charToConvert = charToConvert.toLowerCase();
+            return (alphaMorse.get(charToConvert));
         } else {
-            return "    ";
+            return alphaMorse.inverse().get(charToConvert);
         }
     }
 }
