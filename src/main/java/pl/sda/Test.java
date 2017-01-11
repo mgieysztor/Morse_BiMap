971f4efbe3c8f8ff90e56e200a0c5870d7a89997
mgieysztor <michal.gieysztor@gmail.com>
2017-01-11 20:33:29 +0100

    Dodana klasa do testów visualVM.

---
+++ src/main/java/pl/sda/Test.java
@@ -0,0 +1,10 @@
+package pl.sda;
+
+/**
+ * Created by RENT on 2017-01-11.
+ */
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(120000);
+    }
+}
